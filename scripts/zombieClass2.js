57c3ea4dcb63da79a2a0d92936a7e3861a8a6192
Travis Hairfield <twh116@gmail.com>
2013-04-28 21:18:13 -0400

    Added Zombie2 and level3
    Signed-off-by: Travis Hairfield <twh116@gmail.com>

---
+++ scripts/zombieClass2.js
@@ -0,0 +1,53 @@
+function Zombie2(number, startx, starty, startNumCycles, audioUrl) {
+  this.name = "zombie" + number;
+  this.audioUrl = audioUrl || "http://cs.unc.edu/~stancill/comp585/sounds/female_scream.ogg";
+  this.loop = true;
+  this.x = startx;
+  this.y = starty;
+  this.rot = 0;
+  this.moveSpeed = 0.4; // How far zombie moves in one move
+  this.moveTime = 1; //How many game cycles it takes for the zombie to move
+  this.numCycles = startNumCycles ? startNumCycles : 0; //The number of cycles since the zombie last moved
+  this.intelligence = 25; //A ratio of how much the zombie follows the player, >1 required
+  this.circleColor = "rgba(100,0,0,0.3)";
+  this.panner = true;
+  this.coneOuterGain = 0.005;
+  this.rolloffFactor = 1;
+  this.isZombie = true;
+}
+
+Zombie2.prototype.move = function(playerX, playerY){
+  
+  z = this;
+
+  z.numCycles = (z.numCycles + 1) % z.moveTime;
+    //console.log(z.numCycles);
+  z.intelligence = z.intelligence / 1.005;
+  console.log(z.intelligence);
+  if(z.intelligence < 20)
+    z.audioUrl = "http://cs.unc.edu/~stancill/comp585/zombie-17.wav"
+ // z.moveSpeed = z.moveSpeed * 1.001;
+
+  if (z.numCycles == 0) {   // only move every once in a while
+    var randChase = Math.floor(Math.random() * z.intelligence);
+    if (randChase == 0)
+      z.rot = Math.round(Math.atan2(playerY - z.y, playerX - z.x) * 10) / 10.0;
+    else
+      z.rot += Math.random() * twoPI / 4 - twoPI / 8;
+    var moveStep = z.moveSpeed; // zombie will move this far along the current direction vector
+
+    // make sure the angle is between 0 and 360 degrees
+    while (z.rot < 0) z.rot += twoPI;
+    while (z.rot >= twoPI) z.rot -= twoPI;
+
+    var newX = z.x + Math.cos(z.rot) * moveStep;  // calculate new zombie position with simple trigonometry
+    var newY = z.y + Math.sin(z.rot) * moveStep;
+
+    if (isBlocking(newX, newY)) { // is the zombie allowed to move to the new position?
+      return; // no, bail out.
+    }
+
+    z.x = newX;
+    z.y = newY;
+  }
+}
